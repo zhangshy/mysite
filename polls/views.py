5e87b98881a2e24d283abb5ba297ec5222889ffc
zhangshy <zaozh1990@163.com>
2015-01-16 00:08:34 +0800

    添加404页面

--- polls/views.py
+++ polls/views.py
@@ -1,18 +1,20 @@
 from django.http import HttpResponse
-from django.template import RequestContext, loader
+from django.http import Http404
+from django.shortcuts import render
 
 from polls.models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = RequestContext(request, {
-        'latest_question_list': latest_question_list,
-    })
-    return HttpResponse(template.render(context))
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404
+    return render(request, 'polls/detail.html', {'question': question})
     
 def results(request, question_id):
     response = "You're looking at the results of question %s."
