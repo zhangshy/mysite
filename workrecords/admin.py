df0f1b0a2114c7c4c6de8bed37cce15ebe3a5645
zhangshy <zaozh1990@163.com>
2015-01-18 00:04:20 +0800

    修改work的显示

--- workrecords/admin.py
+++ workrecords/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from workrecords.models import Work
 
-admin.site.register(Work)
+class WorkAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,              {'fields': ['title']}),
+        ('详情',            {'fields': ['work_text', 'work_path']}),
+        ('备注',            {'fields': ['remarks'], 'classes': ['collapse']}),
+        ('提交日期',         {'fields': ['rec_date'], 'classes': ['collapse']}),
+    ]
+    list_display = ('title', 'work_path', 'rec_date')
+    search_fields = ['title', 'work_text', 'remarks']  #设置从哪几项中搜索
+
+admin.site.register(Work, WorkAdmin)
 
--- workrecords/models.py
+++ workrecords/models.py
@@ -3,11 +3,11 @@ from django.utils import timezone
 
 class Work(models.Model):
     now = timezone.now()
-    title = models.CharField(max_length=100)
-    work_text = models.CharField(max_length=200)
+    title = models.CharField('主题', max_length=100)
+    work_text = models.CharField('内容', max_length=200)
     rec_date = models.DateTimeField('date recorded', default=now)
-    work_path = models.CharField(max_length=100)
+    work_path = models.CharField('目录', max_length=100)
     remarks = models.CharField(max_length=200, blank=True, null=True)
 
     def __str__(self):
-        return self.work_text
+        return self.title
